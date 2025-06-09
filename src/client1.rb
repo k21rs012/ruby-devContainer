07dfcbcaf8aea007f434ba3f01e16d28124a5ddd
Syun Irikida <syun@smkwlab.net>
2025-06-09 11:51:39 +0900

    ヘッダー情報を表示しないよう改良

--- src/client1.rb
+++ src/client1.rb
@@ -1,22 +1,23 @@
 #! /usr/local/bin/ruby
 
 require 'socket'
-# host = ARGV[0]
-# path = ARGV[1]
 host, path = ARGV
 
 # ./client1.rb host pathで実行できるようにする
+# ヘッダー情報を表示しないように改良
 
 s = TCPSocket.open(host, "http")
-# s.puts "GET / HTTP/1.0\r\n\r\n"
-# s.puts "Host: www.is.kyusan-u.ac.jp"
-# s.print("GET / HTTP/1.0\r\n")
-# s.print("GET /~toshi/ HTTP/1.1\r\n")
 s.print("GET #{path} HTTP/1.1\r\n")
 s.print("Host: #{host}\r\n")
 s.print("Connection: close\r\n")
 s.print("\r\n")
 
+# レスポンスのヘッダーを読み飛ばす
+while line = s.gets&.chomp
+  break if line.empty?
+end
+
+# ヘッダーを読み飛ばした後、ボディ部分を出力する
 while line = s.gets&.chomp
   puts line
 end
