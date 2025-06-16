a91e88da899d6355bef2a6334defc06570e70d07
Syun Irikida <syun@smkwlab.net>
2025-06-16 12:25:29 +0900

    無限ループ処理に変更、マルチスレッド処理に変更

--- src/server1.rb
+++ src/server1.rb
@@ -4,12 +4,18 @@ require "socket"
 def server sock
   while buf = sock.gets
     p buf
+    sock.puts "reply:#{buf}"
   end
   sock.close
 end
 
 # ポート80で待ち受け
 s0 = TCPServer.open(80)
-sock = s0.accept
-server(sock)
+# 終了しないようにしたい
+while true
+  sock = s0.accept
+  Thread.new do
+    server(sock)
+  end
+end
 s0.close
