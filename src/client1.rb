9f007861d016259b9aabf8a5e9debf9ff4347fe3
Syun Irikida <syun@smkwlab.net>
2025-06-23 12:30:21 +0900

    上げ忘れ

--- src/client1.rb
+++ src/client1.rb
@@ -1,10 +1,22 @@
 #! /usr/local/bin/ruby
 
 require 'socket'
-host, path = ARGV
+require 'uri'
+# コマンドライン引数からURLを取得
+
+url = ARGV[0]
+# uri = URI.parse(url)
+# host = uri.host
+# path = uri.path
+
+url = url[7..]
+host_index = url.index('/')
+host = url[0,host_index]
+path = url[host_index..]
 
 # ./client1.rb host pathで実行できるようにする
 # ヘッダー情報を表示しないように改良
+# URLをhttp://example.com/pathのように指定できるようにする
 
 s = TCPSocket.open(host, "http")
 s.print("GET #{path} HTTP/1.1\r\n")
